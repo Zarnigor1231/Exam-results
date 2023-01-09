6d22dadaf60f3fe5fc9b67a4f5da08d8c7008962
Zarnigor1231 <ZarnigorOvhilovaGitHup@gmaail.com>
2023-01-09 19:31:50 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 // const studentTemplate = document.querySelector('#student-template')
 // const failPass = document.querySelector('.student-pass-status')
+// const inpnum = document.querySelector('.inpnum')
 
 const addForm = document.querySelector('#add-form')
 const addModal = document.querySelector('#add-student-modal')
+const studentsTableBody = document.querySelector('#students-table-body');
 const addElModal = new bootstrap.Modal(addModal)
 const count = document.querySelector('.count')
+const textEnd = document.querySelector('.text-end')
+const filter = document.querySelector('.filter')
+const search = document.querySelector('#search')
+const formto = document.querySelector('.formto')
+const from = document.querySelector('#from')
+const to = document.querySelector('#to')
+const formSelect = document.querySelector('.form-select')
+const studentEdit = document.querySelector('.student-edit')
+const edit = document.querySelector('.edit')
+
 
 
 const highScore = 150;
@@ -14,14 +26,14 @@ const lowScore =40
 
 const addDate = function(number){
     return number < 10 ? '0'+ number : number
-}
+};
 
 
 const showDate = function(dateString){
     const date = new Date(dateString)
     return `${addDate(date.getDate())}.${addDate(date.getMonth()+1)}.${addDate(date.getFullYear())} ${addDate(date.getHours()+1)}:${addDate(date.getMinutes()+1)}`
     
-}
+};
 
 const renderStudent = function(student){
 
@@ -51,29 +63,69 @@ const renderStudent = function(student){
         passStatus.textContent = 'Fail'
         passStatus.classList.add('text-danger')
     }
+    // console.log(everPercent);
+
 
     return studentsTable
-}
+};
 
-const studentsTableBody = document.querySelector('#students-table-body');
 
-const renderStudents = function(){
+const renderStudents = function(search){
 
     studentsTableBody.innerHTML = ''
 
+     // COUNT
+    count.textContent = `Count: ${students.length}`
+
     const statusFragment = document.createDocumentFragment();
 
-       students.forEach(student => {
-            const studentsTable = renderStudent(student)
-            statusFragment.append(studentsTable)
-        });
+        if(search && search.length){
+            search.forEach(student => {
+                const studentsTable = renderStudent(student)
+                statusFragment.append(studentsTable)
+            });
+        }
+        // else if(formSelect.value==1){
+        //     search.sort()
+        // }else if (formSelect==2){
+        //     search.sort((a,b)=> a.Mark - b.Mark)
+        // }
+        // else if(formSelect==3){
+        //     search.sort((a,b)=> b.Mark - a.Mark)
+        // }
+        // else if(formSelect==4){
+        //     search.sort((a,b)=> a.MarkedDate - b.MarkedDate)
+        // }
+        else{
+            students.forEach(student => {
+                const studentsTable = renderStudent(student)
+                statusFragment.append(studentsTable)
+            });
+        }
+   
+        
+
         studentsTableBody.append(statusFragment)
         
     
-}
+};
 
-count.textContent = `Count: ${students.length}`
 
+// textEnd
+
+let textEndarr = [
+       
+]
+
+const initialValue = 0;
+
+textEnd.textContent = `Average mark: ${textEndarr.reduce((a,b)=> +a + +b,initialValue)/students.length} %`
+
+// console.log(textEndarr);
+
+
+
+// addForm add
 
 addForm.addEventListener('submit',(evt) =>{
     evt.preventDefault()
@@ -81,7 +133,7 @@ addForm.addEventListener('submit',(evt) =>{
     const nameValue = evt.target[0].value
     const lastnameValue = evt.target[1].value
     const markValue = evt.target[2].value
-    console.log(lastnameValue)
+    // console.log(lastnameValue)
 
     if(nameValue && lastnameValue && markValue){
         const newStudent = {
@@ -90,20 +142,44 @@ addForm.addEventListener('submit',(evt) =>{
             lastName:lastnameValue,
             MarkedDate:new Date(),
             Mark:markValue
-        }
+     }
+
         
         students.push(newStudent)
+        textEndarr.push(markValue*percent/highScore)
         localStorage.setItem("students",JSON.stringify(students))
-        console.log(nameValue);
+        // console.log(nameValue);
     }
-    console.log(1);
+
+
+
+    console.log(markValue)
+
+  
     evt.target.reset()
     addElModal.hide()
     renderStudents()
 
-})
+});
 
 renderStudents()
 
 
 
+filter.addEventListener('submit',function(e){
+    e.preventDefault()
+
+
+    let filterStudent = students.filter(item => item.name.toLowerCase().includes(search.value.toLowerCase()) &&
+    ( ((item.Mark*percent/highScore >= from.value)|| (from.value=="")) && ((item.Mark*percent/highScore<=to.value) || (to.value=="")) ))
+    // console.log(filterStudent)
+    // console.log(from.value)
+    // console.log(to.value)
+    // console.log(students[3].Mark)
+    renderStudents(filterStudent)
+ });
+
+
+
+
+ 
